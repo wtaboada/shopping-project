12d079f0e7028403c03068bb91a330be7ebf2a60
Wendolin Taboada <wendolin.taboada@netlogistik.com>
2017-07-01 19:15:35 -0500

    Finish all new functionalities with Services

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,16 +1,41 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from "../shared/ingredient.model";
+import {ShoppingListService} from "../shopping-list/shopping-list.sevice";
 
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-superJumbo.jpg'),
-        new Recipe('A Test Recipe 2', 'This is simply a test 2', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-superJumbo.jpg')
+        new Recipe(
+          'A Test Recipe',
+          'This is simply a test',
+          'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-superJumbo.jpg',
+          [
+            new Ingredient('Meat', 1),
+            new Ingredient('French Fries', 20)
+          ]),
+        new Recipe(
+          'A Test Recipe 2',
+          'This is simply a test 2',
+          'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-superJumbo.jpg',
+          [
+            new Ingredient('Buns', 2),
+            new Ingredient('Meat', 1)
+          ])
     ];
+
+    constructor(private shoppingListService: ShoppingListService) {
+
+    }
 
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+      this.shoppingListService.addIngredients(ingredients);
     }
 }
--- src/app/shopping-list/shopping-list.sevice.ts
+++ src/app/shopping-list/shopping-list.sevice.ts
@@ -5,7 +5,7 @@ import { Ingredient } from '../shared/ingredient.model';
 export class ShoppingListService {
     ingredientsChanged = new EventEmitter<Ingredient[]>();
 
-    ingredients: Ingredient[] = [
+    private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 10)
     ];
@@ -17,5 +17,18 @@ export class ShoppingListService {
     createIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
         this.ingredientsChanged.emit(this.ingredients.slice())
+    }
+
+    addIngredients(ingredients: Ingredient[]) {
+      // ingredients.forEach((ingredient: Ingredient) => {
+      //   this.ingredients.push(ingredient);
+      // })
+
+      // for (let ingredient of ingredients) {
+      //   this.createIngredient(ingredient);
+      // }
+
+      this.ingredients.push(...ingredients);
+      this.ingredientsChanged.emit(this.ingredients.slice());
     }
 }
